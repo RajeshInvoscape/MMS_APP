1265d132e00b91382482c86490cb51bb6019bfa5
Rajesh <rajesh.newbie@invoscape.com>
2013-07-15 11:02:29 +0530

    first stage 1

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	 $('#create_project').hide();
-   $('#new_project').click(function(){
+   $('.new_project').click(function(){
       $('#create_project').show();
    });
 });
